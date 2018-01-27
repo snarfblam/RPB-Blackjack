22a17524048418e41bad64bce8f2479ab394952c
Tom H <snarfblam@gmail.com>
2018-01-27 17:25:19 -0500

    Initial file structure

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+//@ts-check
+
+var RpbGame;
+
+$(document).ready(function () {
+    var rpbGame = {
+        init: function(){
+
+        },
+    };
+
+    RpbGame = rpbGame;
+    RpbGame.init();
+});
